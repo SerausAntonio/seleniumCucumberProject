a53838f18a00e0b7b584dc262f8a6d56e37ef164
AntonioSeraus <aseraus@hotmail.com>
2022-03-26 22:34:02 +0100

    Fixed bug stepdefinions

--- src/test/java/stepDefinitions/OrangeHRMsteps.java
+++ src/test/java/stepDefinitions/OrangeHRMsteps.java
@@ -1,6 +1,9 @@
 package stepDefinitions;
 
-import cucumber.api.java.en.*;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class OrangeHRMsteps {
 
