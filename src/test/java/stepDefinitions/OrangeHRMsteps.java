c6c8deebf6d7635b895f0e6d636d4000cd502cab
AntonioSeraus <aseraus@hotmail.com>
2022-03-27 00:00:08 +0100

    Remote driver added

--- src/test/java/stepDefinitions/OrangeHRMsteps.java
+++ src/test/java/stepDefinitions/OrangeHRMsteps.java
@@ -4,11 +4,35 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.remote.Browser.CHROME;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class OrangeHRMsteps {
 
+    DesiredCapabilities cap = new DesiredCapabilities();
+
+
+    RemoteWebDriver driver;
+
+
+
     @Given("I launch chrome browser")
     public void i_launch_chrome_browser() {
+      cap.setBrowserName(String.valueOf(CHROME));
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         // Write code here that turns the phrase above into concrete actions
        System.out.println("I launche the chrome browser");
     }
